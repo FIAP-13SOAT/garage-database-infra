e4daf8f3cb51e0cc09c21fdc0923e24492497821
João Pedro Truchinski Borba <joao.borba@asaas.com.br>
2026-05-17 20:24:07 -0300

    Revert "feat: rmv document db"
    This reverts commit a273533f8ff3bb667dab226d8e8a213dead353e0.

--- infra/security_groups.tf
+++ infra/security_groups.tf
@@ -27,3 +27,30 @@ resource "aws_security_group" "rds" {
     }
 }
 
+########################################
+# SECURITY GROUP - DocumentDB (MongoDB)
+########################################
+
+resource "aws_security_group" "docdb" {
+    name_prefix = "${local.projectName}-docdb-sg"
+    vpc_id      = aws_vpc.main.id
+
+    ingress {
+        from_port   = 27017
+        to_port     = 27017
+        protocol    = "tcp"
+        cidr_blocks = [aws_vpc.main.cidr_block]
+        description = "Allow MongoDB access from within VPC"
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "${local.projectName}-docdb-security-group"
+    }
+}
